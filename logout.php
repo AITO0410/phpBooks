d7fb2e6b2629ade42488c828e816ed8f1d5cf105
ZahidHasan90 <230052sc@yse-c.net>
2024-03-13 11:25:57 +0900

    ログアウト　（仮）

--- logout.php
+++ logout.php
@@ -3,9 +3,13 @@
 【機能】
 	セッション情報を削除しログイン画面に遷移する。
 */
+
 //①セッションを開始する。
-
+session_start();
+$_SESSION = array();
 //②セッションを削除する。
-
+session_destroy();
 //③ログイン画面へ遷移する。
+header("Location: login.php");
+exit;
 ?>
